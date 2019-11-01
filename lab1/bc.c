3d417017139d853621aa196755cfe34b643f1596
Cuizhenghao520 <1178929971@qq.com>
2019-10-31 22:18:12 -0700

    ex2 finished

--- lab1/bc.c
+++ lab1/bc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int n=0;
+    int i=0;
     int n1=0;
     int n2=0;
     scanf("%d",&n2);
@@ -15,7 +15,6 @@ int main()
 printf("%d\n",i);
 return 0;
 }
-
 
 
 
