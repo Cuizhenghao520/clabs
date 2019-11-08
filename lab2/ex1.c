863695310fee47ce5dc81c73688e290247c04d81
Cuizhenghao520 <1178929971@qq.com>
2019-11-07 23:59:01 -0800

    addded a ex1 file

---
+++ lab2/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    unsigned int a=1;
+    unsigned int q=0-1;
+    int p=0;
+     while(a !=0){ 
+         a=a<<1;
+         p=p+1;
+     }
+     printf("a=%d\n",p);
+     printf("p=%u",q);
+     return 0; 
+}
+
+
+
+    
+
+
+
+
