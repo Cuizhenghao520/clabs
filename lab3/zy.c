d2bc4ef8716bd306bc7c4a931e66fea389da4a14
Cuizhenghao520 <1178929971@qq.com>
2019-11-14 07:33:52 -0800

    added a zy.c file

--- lab3/zy.c
+++ lab3/zy.c
@@ -2,11 +2,11 @@
 #include<string.h>
 int strindex (char s[], char t[])
 {
-    int e,f,g,pos=0;
-    for(e = 0;s[e] !='\0'; e++){
-        for(f = e,g=0;t[g]!='\0'&& s[f]==t[g];f++,g++);
+    int m,n,g,pos=0;
+    for(m = 0;s[m] !='\0'; m++){
+        for(n = m,g=0;t[g]!='\0'&& s[n]==t[g];n++,g++);
         if(t[g]=='\0'){
-        pos = e+1;
+        pos = m+1;
     }
     }
     if(pos >0)
